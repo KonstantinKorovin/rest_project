2a328e9cafd756619b66f09934b809ae271e3b38
Konstantin Korovin <ryan337@mail.ru>
2025-09-21 16:12:02 +0300

    start-develop

---
+++ users/models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(verbose_name="E-mail пользователя", unique=True)
+    phone_number = models.CharField(
+        max_length=15, verbose_name="Номер телефона", blank=True, null=True, unique=True
+    )
+    city = models.CharField(verbose_name="Город пользователя", blank=True, null=True)
+    avatar = models.ImageField(
+        verbose_name="Фотография пользователя",
+        upload_to="users/photo/",
+        default="users/photo/default.png",
+        blank=True,
+        null=True,
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = [
+        "username",
+    ]
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
