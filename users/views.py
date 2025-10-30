00e2c926ad69a484fd9b16aebadd97d575fd018a
Konstantin Korovin <ryan337@mail.ru>
2025-10-30 16:07:48 +0300

    sev-sev

--- users/views.py
+++ users/views.py
@@ -190,3 +190,5 @@ class PaymentCancelView(generics.RetrieveAPIView):
             {"message": "Оплата отменена пользователем."},
             status=status.HTTP_400_BAD_REQUEST,
         )
+
+###########################
