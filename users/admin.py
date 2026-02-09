269e635983f0afea4e147ede78a5e47a7264eb90
KonstantinKorovin <korovin521337@gmail.com>
2026-02-09 13:22:52 +0300

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,1 +1,10 @@
-# Create 
+from django.contrib import admin
+
+from users.models import CustomUser
+
+
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    fields = ("email", "phone_number", "city")
+    search_fields = ("email", "city")
+    ordering = ("email",)
